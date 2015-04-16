f673602498b9f64e73ecbe385576d43656c01eb5
Hackbright Student <info@hackbrightacademy.com>
2015-04-16 13:03:25 -0700

    Initializing repo

---
+++ markov.py
@@ -0,0 +1,38 @@
+import sys
+from random import choice
+
+
+class SimpleMarkovGenerator(object):
+
+    def read_files(self, filenames):
+        """Given a list of files, make chains from them."""
+        
+        source_text = ""
+
+        for file in filenames:
+            source_text += file.read()
+
+        print source_text
+
+    def make_chains(self, corpus):
+        """Takes input text as string; stores chains."""
+
+        # your code here
+
+    def make_text(self):
+        """Takes dictionary of markov chains; returns random text."""
+
+        # your code here
+
+
+if __name__ == "__main__":
+
+    # we should get list of filenames from sys.argv
+    # we should make an instance of the class
+    # we should call the read_files method with the list of filenames
+    # we should call the make_text method 5x
+
+    pass
+
+m=SimpleMarkovGenerator()
+m.read_files(sys.argv[1:])
