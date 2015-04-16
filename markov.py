6ba89733135a10dade47318638e307baffe2f6a1
Hackbright Student <info@hackbrightacademy.com>
2015-04-16 16:32:28 -0700

    completed read_files and make_chains methods

--- markov.py
+++ markov.py
@@ -32,13 +32,29 @@ class SimpleMarkovGenerator(object):
             if i <= len(source_list)-3:
                     markov_dict[(source_list[i], source_list[i+1])].append(source_list[i+2])
 
-        print markov_dict
         return markov_dict
 
     def make_text(self):
         """Takes dictionary of markov chains; returns random text."""
 
-        # your code here
+        #Start with a random bi-gram
+        bi_gram = random.choice(chains.keys())
+        
+        #Start output_string with our first bi-gram
+        output_string = ""
+        output_string = starter_string + " ".join(bi_gram)
+
+        #Continue generating random text, concatonating to output_string, and creating new bi-grams as we go
+        while True:
+            if chains[bi_gram] != []:
+            #if bi_gram in chains and chains[bi_gram] != []:
+                new_word = random.choice(chains[bi_gram])
+                output_string += " " + new_word
+                bi_gram = (bi_gram[1], new_word)
+            else:
+                break
+
+        return output_string
 
 
 if __name__ == "__main__":
@@ -46,7 +62,7 @@ if __name__ == "__main__":
     #Instantiating the class
     m=SimpleMarkovGenerator()
     source_string = m.read_files(sys.argv[1:])
-    m.make_chains(source_string)
+    m.make_chains(source_string).make_text()
 
     # we should get list of filenames from sys.argv
     # we should make an instance of the class
